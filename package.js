9e47c9cc94f698eb0799b4f78127c7438fede2ec
Jonathan Azulay <jonathanazulay@me.com>
2016-02-26 22:29:38 +0100

    add deps

--- package.js
+++ package.js
@@ -1,7 +1,6 @@
 // package metadata file for Meteor.js
 
 /* global Package:true */
-
 Package.describe({
   name: 'jonathanazulay:bootstrap',  // http://atmospherejs.com/twbs/bootstrap
   summary: 'The most popular front-end framework for developing responsive, mobile first projects on the web.',
@@ -11,8 +10,14 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0');
-  api.use('jquery', 'client');
+
   api.use('fourseven:scss');
+  api.use('jquery', 'client');
+
+  api.addFiles([
+      'tether.js',
+      'dist/js/bootstrap.js'
+    ], 'client');
 
   api.addFiles([
       'scss/_alert.scss',
